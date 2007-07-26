a18dd43bebf9f3f40a0e7da5d12f1960f4a01d48
Leni Mayo <github.com@moniker.net>
2007-07-26 20:59:32 +0000

    build.sh

--- build-config.sh
+++ build-config.sh
@@ -3,10 +3,10 @@
 # Build config for the build script, build.sh. Look there for more info.
 
 APP_NAME=zindus
+APP_VERSION_NUMBER=`perl -e 'while (<>) { if (/^\s*\<em:version\>(.*)\<\/em:version\>/) { print $1 . "\n"; } }' < install.rdf`
 CHROME_PROVIDERS="content locale"
 CLEAN_UP=1
 ROOT_FILES="README"
-ROOT_DIRS="components defaults"
-BEFORE_BUILD=
+ROOT_DIRS="defaults"
+BEFORE_BUILD=./sed-script.sh
 AFTER_BUILD=
-VERSION_NUMBER=`perl -e 'while (<>) { if (/^\s*\<em:version\>(.*)\<\/em:version\>/) { print $1 . "\n"; } }' < install.rdf`
