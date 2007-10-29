41cba566ebc1a4d6fba088e3059a58806af39000
Leni Mayo <github.com@moniker.net>
2007-10-29 23:53:48 +0000

    *** empty log message ***

--- build-after.sh
+++ build-after.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# $Id: build-after.sh,v 1.1 2007-10-08 22:30:25 cvsuser Exp $
+# $Id: build-after.sh,v 1.2 2007-10-29 23:53:48 cvsuser Exp $
 
 
 if [ "$APP_VERSION_RELTYPE" = "testing" -o "$APP_VERSION_RELTYPE" = "prod-zindus" ]; then
   scp -q $XPI_FILE_NAME spring.moniker.net:/home/httpd/zindus.com/htdocs/download/xpi
-  rm $XPI_FILE_NAME
+  # rm $XPI_FILE_NAME
   echo $XPI_FILE_NAME copied to spring.moniker.net
 fi
--- build-config.sh
+++ build-config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# $Id: build-config.sh,v 1.11 2007-10-29 23:53:48 cvsuser Exp $
+# $Id: build-config.sh,v 1.12 2007-10-29 23:54:14 cvsuser Exp $
 
 # Build config for the build script, build.sh. Look there for more info.
 
 APP_NAME=zindus
-APP_VERSION_NUMBER=0.6.3
+APP_VERSION_NUMBER=0.6.4
 APP_VERSION_RELTYPE="testing" # must be one of "testing", "prod-zindus" or "prod-amo"
 # APP_VERSION_RELTYPE="prod-zindus"
 CHROME_PROVIDERS="content locale"
