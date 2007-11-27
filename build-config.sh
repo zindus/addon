7a1738f3716d835340639c4ccc872d71d443366b
Leni Mayo <github.com@moniker.net>
2007-11-27 21:51:04 +0000

    *** empty log message ***

--- build-config.sh
+++ build-config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# $Id: build-config.sh,v 1.17 2007-11-27 21:27:03 cvsuser Exp $
+# $Id: build-config.sh,v 1.18 2007-11-27 21:51:04 cvsuser Exp $
 
 # Build config for the build script, build.sh. Look there for more info.
 
 APP_NAME=zindus
-APP_VERSION_NUMBER=0.6.7.3
+APP_VERSION_NUMBER=0.6.8
 # APP_VERSION_RELTYPE="testing" # must be one of "testing", "prod-zindus" or "prod-amo"
 APP_VERSION_RELTYPE="prod-zindus"
 CHROME_PROVIDERS="content locale"
