7a2e50878b43bdc7eddac9eeb0958e544ffaa8a5
Leni Mayo <github.com@moniker.net>
2007-12-02 07:09:32 +0000

    changed branch from zindus.blah to extensions.zindus.blah

--- defaults/preferences/zindus.js
+++ defaults/preferences/zindus.js
@@ -1,5 +1,5 @@
-pref("zindus.system.SyncGalMdInterval",      86400);    // how frequently the entire GAL is requested (seconds) (use 60 for debugging)
-pref("zindus.system.timerDelayOnStart",      3600);     // 3600  how long before the sync timer fires after startup (seconds)
-pref("zindus.system.timerDelayOnRepeat",     43200);    // 300 86400 how long before the second and subsequent sync timers fire
-pref("zindus.system.preferSchemeForSoapUrl", "https");  // which scheme to prefer if multiple <soapURL>'s are recieved
-pref("zindus.system.logfileSizeMax",         10000000); // logfile is truncated when it gets bigger than this
+pref("extensions.zindus.system.SyncGalMdInterval",      86400);    // 60   how frequently the entire GAL is requested (seconds)
+pref("extensions.zindus.system.timerDelayOnStart",      3600);     // 3600 how long before the sync timer fires after startup (seconds)
+pref("extensions.zindus.system.timerDelayOnRepeat",     43200);    // 300 86400 how long before the second and subsequent sync timers fire
+pref("extensions.zindus.system.preferSchemeForSoapUrl", "https");  // which scheme to prefer if multiple <soapURL>'s are recieved
+pref("extensions.zindus.system.logfileSizeMax",         10000000); // logfile is truncated when it gets bigger than this
