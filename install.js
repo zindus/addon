acbb41ab63a32ce997b36ba9b5413675daae204a
Leni Mayo <github.com@moniker.net>
2008-10-26 04:11:03 +0000

    *** empty log message ***

--- install.js
+++ install.js
@@ -8,7 +8,7 @@
 
 // Editable Items Begin
 var displayName         = "Zindus"; // The name displayed to the user (don't include the version)
-var version             = "0.8.2";
+var version             = "0.8.2.20081026.150550";
 var name                = "zindus"; // The leafname of the JAR file (without the .jar part)
 
 // The following three sets of variables tell this installer script how your
