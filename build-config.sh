62f89929a54790e50c89789b5f2a412432d0b79c
Leni Mayo <github.com@moniker.net>
2010-09-14 04:49:55 +0000

    *** empty log message ***

--- build-config.sh
+++ build-config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-# $Id: build-config.sh,v 1.68 2010-09-06 05:21:39 cvsuser Exp $
+# $Id: build-config.sh,v 1.69 2010-09-14 04:49:55 cvsuser Exp $
 
 # Build config for the build script, build.sh. Look there for more info.
 
 APP_NAME=zindus
-APP_VERSION_NUMBER=0.8.25
+APP_VERSION_NUMBER=0.8.27
 CHROME_PROVIDERS="content locale skin"
 CLEAN_UP=1
 ROOT_FILES="README"
