2640482f53900c1c85fe8f6598becc26f6db1dac
Leni Mayo <github.com@moniker.net>
2011-07-09 16:11:06 +1000

    remove unnecessary whitespace

--- content/zindus/contactconverter.js
+++ content/zindus/contactconverter.js
@@ -1,24 +1,24 @@
 /* ***** BEGIN LICENSE BLOCK *****
- * 
+ *
  * "The contents of this file are subject to the Mozilla Public License
  * Version 1.1 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
  * http://www.mozilla.org/MPL/
- * 
+ *
  * Software distributed under the License is distributed on an "AS IS"
  * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
  * License for the specific language governing rights and limitations
  * under the License.
- * 
+ *
  * The Original Code is Zindus Sync.
- * 
+ *
  * The Initial Developer of the Original Code is Toolware Pty Ltd.
  *
  * Portions created by Initial Developer are Copyright (C) 2007-2009
  * the Initial Developer. All Rights Reserved.
- * 
+ *
  * Contributor(s): Leni Mayo
- * 
+ *
  * ***** END LICENSE BLOCK *****/
 
 includejs("crc32.js");
@@ -163,7 +163,7 @@ setup : function(style) {
 	this.m_dont_convert[FORMAT_GD] = { };
 	this.m_dont_convert[FORMAT_ZM] = newObjectWithKeys(
 		// eg. the <cn> elements returned by SyncGal include ldap attributes
-		// 
+		//
 		"zimbraId", "objectClass", "createTimeStamp", "zimbraMailForwardingAddress", "zimbraCalResType", "modifyTimeStamp",
 		//
 		// these attributes aren't in the zimbra web ui, they are added by the zimbra outlook client
