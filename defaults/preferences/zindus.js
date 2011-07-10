d86e3cccddec37e8e1b14b625f0ee433232a5296
Leni Mayo <github.com@moniker.net>
2011-07-10 19:59:45 +1000

    tidy up comments

--- defaults/preferences/zindus.js
+++ defaults/preferences/zindus.js
@@ -9,20 +9,19 @@ pref("extensions.zindus.system.as_timer_delay_on_start",  3600);     // how long
 pref("extensions.zindus.system.as_timer_delay_on_repeat", 43200);    // how long before the second and subsequent sync timers fire
 pref("extensions.zindus.system.as_allow_pre_release",     true);     // allow the addon to run in a pre-release Thunderbird
 pref("extensions.zindus.system.as_password_version",      "notset"); // pm-2, lm-2
-pref("extensions.zindus.system.as_share_service_api_url", "xxx");    // 
+pref("extensions.zindus.system.as_share_service_api_url", "xxx");    //
 pref("extensions.zindus.system.as_migration.0",           "0:BirthDay,BirthMonth,BirthYear,WebPage1,WebPage2");
 pref("extensions.zindus.system.as_show_again_slow_sync",  true);
 pref("extensions.zindus.system.as_chunk_size_scale",      100);
 pref("extensions.zindus.system.zm_sync_gal_md_interval",  604800);   // how often the entire GAL is requested
 pref("extensions.zindus.system.zm_sync_gal_recheck",      2);        // how many zm_sync_gal_md_interval's before if_fewer is retested
 pref("extensions.zindus.system.zm_sync_gal_if_fewer",     500);      // sync gal if it contains less than this many contacts
-pref("extensions.zindus.system.zm_prefer_soapurl_scheme", "https");  // the scheme to prefer if multiple <soapURL>'s are recieved
-pref("extensions.zindus.system.zm_share_service_url",     "xxx");    // 
-pref("extensions.zindus.system.gd_data_transfer_scheme",  "https");  // the scheme to use for Google Contacts get+set (auth always https)
+pref("extensions.zindus.system.zm_prefer_soapurl_scheme", "https");  // scheme to prefer if multiple <soapURL>'s are recieved
+pref("extensions.zindus.system.zm_share_service_url",     "xxx");    //
+pref("extensions.zindus.system.gd_data_transfer_scheme",  "https");  // scheme to use for Google Contacts get+set (auth always https)
 pref("extensions.zindus.system.gd_trash_expire_seconds",  604800);   // cards in zindus/ToBeDeleted older than this are deleted
 pref("extensions.zindus.system.gd_contacts_per_request",  3000);     // given as an argument to ?max-results=nnnn
-pref("extensions.zindus.system.gd_confirm_on_erase",      true);     // require user intervention if all google contacts are to be deleted
-
+pref("extensions.zindus.system.gd_confirm_on_erase",      true);     // user must confirm if most/all google contacts are to be deleted
 
 pref("extensions.zindus.preauth.0.name",                  "free.fr");
 pref("extensions.zindus.preauth.0.regexp",                "^https?://zimbra\.free\.fr\/?");
